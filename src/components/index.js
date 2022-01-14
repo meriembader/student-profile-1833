6384d23dfbf1ee0175775583a71df856c861cc5a
meriembader <meriembader8@gmail.com>
2022-01-14 16:12:37 +0100

    done

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import StudentProfileList from './StudentProfileList'
+import StudentProfileItem from './StudentProfileItem'
+import TagManager from './TagManager'
+
+export {
+    StudentProfileList,
+    StudentProfileItem,
+    TagManager
+}
